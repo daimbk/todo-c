fe285373894d2fdb4dbf60d037781050d972e85f
daimbk <daimbkhalid@gmail.com>
2024-02-07 21:06:23 +0500

    error msgs

--- src/main.c
+++ src/main.c
@@ -8,11 +8,17 @@
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		printf("Incorrect command.\nUse '<todo help>' to see all commands");
+		printf("Use '<todo help>' to see all commands");
 		exit(1);
 	}
 
 	if (strcmp(argv[1], "add") == 0) {
+		if (argc < 3) {
+			printf("Not enough arguments!");
+			printf("Usage: todo add <task>");
+			exit(1);
+		}
+
 		int mem = 0;
 		char *task = (char *)malloc(mem);
 
@@ -30,10 +36,23 @@ int main(int argc, char *argv[])
 	}
 
 	if (strcmp(argv[1], "delete") == 0) {
+		if (argc < 3 && argc != 3) {
+			printf("Not enough arguments!");
+			printf("Usage: todo delete <task number>");
+			exit(1);
+		}
+
 		delete (atoi(argv[2]));
+		exit(0);
 	}
 
 	if (strcmp(argv[1], "list") == 0) {
+		if (argc != 2) {
+			printf("Incorrect arguments!");
+			printf("Usage: todo list");
+			exit(1);
+		}
+
 		list();
 		exit(0);
 	}
